3070799ce410817cfafa38ac7961b8b968a7f384
Tin Valinčić <tin.valincic@minus5.hr>
2021-03-10 10:11:46 +0100

    feat: create fake api

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import full from "./mock/full.json";
+import diff1 from "./mock/diff1.json";
+import diff2 from "./mock/diff2.json";
+import diff3 from "./mock/diff3.json";
+import diff4 from "./mock/diff4.json";
+import diff5 from "./mock/diff5.json";
+import diff6 from "./mock/diff6.json";
+import diff7 from "./mock/diff7.json";
+import diff8 from "./mock/diff8.json";
+import diff9 from "./mock/diff9.json";
+import diff10 from "./mock/diff10.json";
+const diffs = [
+  diff1,
+  diff2,
+  diff3,
+  diff4,
+  diff5,
+  diff6,
+  diff7,
+  diff8,
+  diff9,
+  diff10,
+];
+
+export const client = {
+  fetchOffer() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(full);
+      }, 100);
+    });
+  },
+  subscribe(fn) {
+    var counter = 0;
+    const interval = setInterval(() => {
+      if (!diffs[counter]) {
+        clearInterval(interval);
+        return;
+      }
+      fn(diffs[counter]);
+      counter++;
+    }, 1000);
+  },
+};
