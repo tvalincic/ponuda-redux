59675a01367b69e5f47de7e23751e2a8c1fc8dfe
Tin Valinčić <tin.valincic@minus5.hr>
2021-03-11 15:22:25 +0100

    refactor(offer): move offer fetching to offer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
 import { Slip } from "./features/slip";
-import { fetchOffer, Offer } from "./features/offer";
+import { Offer } from "./features/offer";
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchOffer());
-  }, [dispatch]);
   return (
     <div className="app">
       <Offer />
--- src/features/offer/components/offer.jsx
+++ src/features/offer/components/offer.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchOffer } from "../state/offerSlice";
 import { CenterSection } from "./center-section";
 import { Sidebar } from "./sidebar";
 
-export const Offer = () => (
-  <section className="offer">
-    <Sidebar />
-    <CenterSection />
-  </section>
-);
+export const Offer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchOffer());
+  }, [dispatch]);
+  return (
+    <section className="offer">
+      <Sidebar />
+      <CenterSection />
+    </section>
+  );
+};
