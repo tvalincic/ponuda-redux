300aea375615da0e2373172e1e165395dc2faa66
Tin Valinčić <tin.valincic@minus5.hr>
2021-03-15 16:11:17 +0100

    fix(offer): update full and diff message mocks

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,5 @@
 import full from "./mock/full.json";
-import diff1 from "./mock/diff1.json";
-import diff2 from "./mock/diff2.json";
-import diff3 from "./mock/diff3.json";
-import diff4 from "./mock/diff4.json";
-import diff5 from "./mock/diff5.json";
-import diff6 from "./mock/diff6.json";
-import diff7 from "./mock/diff7.json";
-import diff8 from "./mock/diff8.json";
-import diff9 from "./mock/diff9.json";
-import diff10 from "./mock/diff10.json";
-const diffs = [
-  diff1,
-  diff2,
-  diff3,
-  diff4,
-  diff5,
-  diff6,
-  diff7,
-  diff8,
-  diff9,
-  diff10,
-];
+import { diffs } from "./mock/diffs";
 
 export const client = {
   fetchOffer() {
---
+++ src/api/mock/diffs.js
@@ -0,0 +1,248 @@
+export const diffs = [
+  {
+    100: {
+      dogadjaji: {
+        264056786: {
+          brojPonuda: 14,
+          ponude: {
+            6: {
+              id: -831607844,
+              stanje: 0,
+              naziv: "dvoznak (1X-12-X2)",
+              poredak: 2,
+              tecajevi: {
+                2: { id: -203294044, naziv: "X2", tecaj: 7, poredak: 2 },
+              },
+              razrada: { _isStruct: 1, id: 2672 },
+            },
+            29: {
+              id: -831610144,
+              stanje: 0,
+              naziv: "regularno vrijeme",
+              poredak: 1,
+              osnovna: true,
+              tecajevi: {
+                0: { id: -203295844, naziv: "1", tecaj: 1.01, poredak: 0 },
+                1: { id: -203295944, naziv: "X", tecaj: 10.5, poredak: 1 },
+                2: { id: -203296044, naziv: "2", tecaj: 20, poredak: 2 },
+              },
+              razrada: { _isStruct: 1, id: 2659 },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    100: {
+      dogadjaji: {
+        264029666: {
+          ponude: {
+            7: {
+              tecajevi: {
+                1: { id: -203296144, tecaj: 1.04 },
+                2: { id: -203296244, tecaj: 3.9 },
+              },
+            },
+            29: {
+              tecajevi: {
+                0: { id: -203296944, tecaj: 1.1 },
+                1: { id: -203297044, tecaj: 4.85 },
+              },
+            },
+          },
+          rezultat: { _isStruct: 1, progres: "12'" },
+        },
+      },
+    },
+  },
+  { 2: { dogadjaji: { 79049162: { poredak: 173 } } } },
+  { 2: { dogadjaji: { 233608436: { poredak: 175 } } } },
+  {
+    100: {
+      dogadjaji: {
+        264056786: {
+          rezultat: { _isStruct: 1, progres: "", period: "", prekid: "HT" },
+        },
+      },
+    },
+  },
+  {
+    4: {
+      dogadjaji: {
+        78756432: {
+          brojPonuda: 17,
+          ponude: { 7: { tecajevi: { 2: { id: -194873444, tecaj: 4.2 } } } },
+          rezultat: {
+            _isStruct: 1,
+            periodi: [
+              [13, 11],
+              [16, 18],
+              [15, 8],
+            ],
+            ukupno: [44, 37],
+          },
+        },
+      },
+    },
+  },
+  {
+    7: {
+      dogadjaji: {
+        79218472: { rezultat: { _isStruct: 1, periodi: [[12, 12]] } },
+      },
+    },
+  },
+  {
+    7: {
+      dogadjaji: {
+        79218472: {
+          ponude: {
+            0: {
+              tecajevi: {
+                0: { id: -201688644, tecaj: 1.37 },
+                1: { id: -201688744, tecaj: 2.6 },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    33: {
+      dogadjaji: {
+        264126866: {
+          ponude: {
+            1: {
+              tecajevi: {
+                0: { id: -203301344, tecaj: 3.8 },
+                1: { id: -200409044, tecaj: 1.2 },
+              },
+            },
+          },
+          rezultat: {
+            _isStruct: 1,
+            periodi: [
+              [6, 11],
+              [9, 11],
+              [13, 11],
+              [11, 6],
+              [4, 7],
+            ],
+          },
+        },
+      },
+    },
+  },
+  {
+    4: {
+      dogadjaji: {
+        78756432: {
+          ponude: {
+            7: {
+              tecajevi: {
+                0: { id: -194616644, tecaj: 1.22 },
+                1: { id: -198268844, tecaj: 13 },
+                2: { id: -203302944, tecaj: 4.8 },
+              },
+            },
+          },
+          rezultat: {
+            _isStruct: 1,
+            periodi: [
+              [13, 11],
+              [16, 18],
+              [15, 9],
+            ],
+            ukupno: [44, 38],
+          },
+        },
+      },
+    },
+  },
+  {
+    101: { dogadjaji: { 264060266: { brojPonuda: 5, ponude: { 39: null } } } },
+  },
+  { 101: { do: { 264048906: { r: { pr: "7'" } } } } },
+  { 2: { do: { 72929322: { po: 175 } } } },
+  { 2: { do: { 73686842: { po: 176 } } } },
+  {
+    100: {
+      do: {
+        264056786: {
+          r: {
+            pe: "2.pol",
+            pi: [
+              [2, 0],
+              [0, 0],
+            ],
+            pk: "",
+            pr: "5'",
+          },
+        },
+      },
+    },
+  },
+  {
+    101: {
+      do: {
+        264048906: { p: { 34: { ti: { 2: { i: -202739544, t: 6.5 } } } } },
+      },
+    },
+  },
+  {
+    101: {
+      do: {
+        264048906: {
+          p: {
+            34: {
+              ti: {
+                0: { i: -202461044, t: 1.15 },
+                1: { i: -202584944, t: 19.5 },
+                2: { i: -202461244, t: 5.5 },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    33: {
+      do: {
+        264036446: {
+          p: {
+            2: {
+              ti: {
+                0: { i: -203315544, t: 3.7 },
+                1: { i: -203170144, t: 1.2 },
+              },
+            },
+          },
+          r: { pi: [[3, 3]], se: 1 },
+          se: 1,
+        },
+      },
+    },
+  },
+  {
+    100: {
+      do: {
+        264029666: {
+          p: {
+            7: { ti: { 2: { i: -203317644, t: 3.95 } } },
+            29: { ti: { 1: { i: -203318344, t: 4.95 } } },
+          },
+        },
+      },
+    },
+  },
+  {
+    100: {
+      do: {
+        264029646: { bp: 0, p: { 6: null, 29: null }, r: { pr: "12'+1" } },
+      },
+    },
+  },
+];
