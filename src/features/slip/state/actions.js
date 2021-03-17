19edc2c4fba526ff914a152bfab77ab9142d1bdf
Tin Valinčić <tin.valincic@minus5.hr>
2021-03-17 15:41:12 +0100

    fix(slip): change all relevant data on stake change

--- src/features/slip/state/actions.js
+++ src/features/slip/state/actions.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { slipApi } from "../../shared/actions";
+import { slipApi, getCurrentSlip } from "../../shared";
 
 export const changeStake = createAsyncThunk(
   "slip/changeStake",
   async (stake) => {
     const ret = slipApi.ulog(stake);
     if (!ret) throw new Error("Failed stake change.");
-    const s = slipApi.listic();
-    console.log(s);
-    return stake;
+    return getCurrentSlip();
   }
 );
